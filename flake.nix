721723fc32daa9376fdbb29bf6f81c6c38c0c842
Gabriela Moreira <gabrielamoreira05@gmail.com>
2026-01-26 15:55:08 -0300

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Quint Trace Explorer development setup";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" "clippy" "rustfmt" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustToolchain
+          ];
+
+          RUST_BACKTRACE = "1";
+        };
+      }
+    );
+}
